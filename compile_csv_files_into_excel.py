c1a1a162da8da89a6fae2dca611833acf08981a8
Amir Jafari <95011376+Jafari-Amir@users.noreply.github.com>
2022-12-28 16:45:29 -0300

    Update compile_csv_files_into_excel.py

--- compile_csv_files_into_excel.py
+++ compile_csv_files_into_excel.py
@@ -16,17 +16,16 @@ for csv_file in csv_files:
   # Split the file path into its individual components
   path_components = csv_file.split('/')
 
-  #remove invalid characters from the file name
+  #remove invalid characters from the file name, in below, before file_name there I added '_' cause every file which containg csv files to be seperated by underscore.
   file_name = path_components[-1]
   file_name = re.sub(r'[\[\]:*?/\\\']', '_', file_name)
 
-  #the name of the folder containing the file
+  #the name of the folder containing the file, underscore have the previously described function.
   folder_name = path_components[-2]
   folder_name = re.sub(r'[\[\]:*?/\\\']', '_', folder_name)
 
   #sheet name by combining the folder name and file name which follows asterstisk rule
   sheet_name = f"{folder_name}_{file_name}"
   df.to_excel(excel_writer, sheet_name=sheet_name)
-
-# Save Excel
+# Save the result
 excel_writer.save()
